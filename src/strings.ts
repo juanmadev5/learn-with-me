90fea737995ee41b0e150fccf2739849c911643b
JM Dev <juanma261005@gmail.com>
2024-11-28 19:14:01 -0300

    some UI fixes

--- src/strings.ts
+++ src/strings.ts
@@ -8,7 +8,7 @@ export const strings = {
   homeMainDescription:
     "Aprende desarrollo web, móvil, y más, ¡todo completamente gratis! Comparte mi viaje de aprendizaje y crecimiento.",
   myExperience: "Te enseño de forma práctica, basada en mi experiencia personal, cubriendo una amplia gama de temas de programación.",
-  blogs: "Blogs",
+  articles: "Artículos",
   topBarTitle: "LearnWithMe",
   by: "by JM Dev",
 };
