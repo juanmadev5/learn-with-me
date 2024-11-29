c529ef056e4d28068e8a89003cd3985f88df9fca
JM Dev <juanma261005@gmail.com>
2024-11-29 11:37:02 -0300

    added strings to strings.ts

--- src/strings.ts
+++ src/strings.ts
@@ -1,14 +1,21 @@
 export const metaDescriptions = {
   mainMetaDesc:
     "Aprende desarrollo web, Android, y Flutter con tutoriales gratuitos y paso a paso para principiantes y expertos.",
-    mainKeywords: "blog programación, programación, código, android, flutter, web"
+  mainKeywords:
+    "blog programación, programación, código, android, flutter, web",
 };
 
 export const strings = {
   homeMainDescription:
     "Aprende desarrollo web, móvil, y más, ¡todo completamente gratis! Comparte mi viaje de aprendizaje y crecimiento.",
-  myExperience: "Te enseño de forma práctica, basada en mi experiencia personal, cubriendo una amplia gama de temas de programación.",
+  myExperience:
+    "Te enseño de forma práctica, basada en mi experiencia personal, cubriendo una amplia gama de temas de programación.",
   articles: "Artículos",
   topBarTitle: "LearnWithMe",
   by: "by JM Dev",
+  publishedDate: "Publicado el: ",
+  publishedBy: "Por: ",
+  at: "A las: ",
+  readTime: "Tiempo de lectura: ",
+  minutes: " minuto(s)",
 };
